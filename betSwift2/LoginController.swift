550fe6e972eec2c658a2ce91fe0a0bdb23d16a7c
stagiaire <stagiaire@iMac15.local>
2017-03-23 09:32:53 +0100

    Login et début de subscribe

--- betSwift2/LoginController.swift
+++ betSwift2/LoginController.swift
@@ -14,6 +14,11 @@ class LoginController: UIViewController {
     @IBOutlet weak var emailTxt: UITextField!
     @IBOutlet weak var passwordTxt: UITextField!
     @IBOutlet weak var errorLbl: UILabel!
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var subscribeBtn: UIButton!
+    @IBOutlet weak var logoutBtn: UIButton!
+    
+    var loginSuccess:BooleanLiteralType = false
     
     let urlString = "http://192.168.56.101/"
 
@@ -23,93 +28,109 @@ class LoginController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        emailTxt.text! = "test@test.com"
-        passwordTxt.text! = "0000"
+        self.loginSuccess = false
 
-        /*let urlString = "http://192.168.56.101/"
-        
-        let userDef = UserDefaults.standard
-        let token:String?  = userDef.object(forKey: "token") as! String?
-
-       
-        if token == nil {
-            let params: Parameters = [
-                "grant_type": "password",
-                "client_id": "1",
-                "client_secret": "ZXdMNAizgOVs27EOqTil3lJGG9z4TIwDy2GdfGxR",
-                "username": "test@test.com",
-                "password": "0000"            ]
-            
-            let headers: HTTPHeaders = ["Accept": "application/json"]
-            
-            
-            Alamofire.request(urlString + "oauth/token", method: .post, parameters: params, encoding: JSONEncoding.default, headers: headers).validate().responseJSON { (response:DataResponse<Any>) in
-                if response.result.isSuccess {
-                    if let result:[String:Any] = response.result.value as? [String:Any] {
-                        let tokenResult = result["access_token"] as? String
-                        print("token  : " + tokenResult!)
-                        UserDefaults.standard.set(tokenResult, forKey:"token")
-                        
-                    }
-                }
-            }
-
-            
-        }else{
-            
-            print("else token  : " + token!)
-        }*/
-        
+        if let name = UserDefaults.standard.string(forKey: "token") {
+            self.disableLogin()
+            print(name)
+        } else {
+            self.showLogin()
+        }
         
     }
     
-
-    @IBAction func login(_ sender: UIButton) {
-        print("email " + emailTxt.text!)
-        print("password " + passwordTxt.text!)
-        
-        
+    func showLogin(){
+        self.logoutBtn.isHidden = true
+        self.emailTxt.isHidden = false
+        self.emailTxt.isHidden = false
+        self.passwordTxt.isHidden = false
+        self.subscribeBtn.isHidden = false
+        self.loginBtn.setTitle("CONNEXION",for: .normal)
+        self.errorLbl.text = "Bienvenue sur BetSwift"
+        emailTxt.text! = "test@test.com"
+        passwordTxt.text! = ""
     }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "mainSegue" {
-            if emailTxt.text != "" && passwordTxt.text != ""  {
-                let params: Parameters = [
-                    "grant_type": "password",
-                    "client_id": "1",
-                    "client_secret": "ZXdMNAizgOVs27EOqTil3lJGG9z4TIwDy2GdfGxR",
-                    "username": emailTxt.text!,
-                    "password": passwordTxt.text!            ]
+    
+    func disableLogin(){
+        self.logoutBtn.isHidden = false
+        self.loginSuccess = true
+        self.emailTxt.isHidden = true
+        self.passwordTxt.isHidden = true
+        self.subscribeBtn.isHidden = true
+        self.loginBtn.setTitle("ENTRER",for: .normal)
+        self.errorLbl.text = "Bienvenue sur BetSwift"
+    }
+
+  
+    @IBAction func doLogin(_ sender: UIButton) {
+        print("doLogin....")
+    }
+    
+    @IBAction func subscribeTouched(_ sender: UIButton) {
+        print("subscribeTouched....")
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String?, sender: Any?) -> Bool {
+        print("shouldPerformSegue")
+        if let ident = identifier {
+            if ident == "login" {
                 
-                let headers: HTTPHeaders = ["Accept": "application/json"]
+                print("Fonction ShouldPerform début...")
                 
-                
-                Alamofire.request(urlString + "oauth/token", method: .post, parameters: params, encoding: JSONEncoding.default, headers: headers).validate().responseJSON { (response:DataResponse<Any>) in
-                    if response.result.isSuccess {
-                        if let result:[String:Any] = response.result.value as? [String:Any] {
-                            let tokenResult = result["access_token"] as? String
-                            print("token  : " + tokenResult!)
-                            UserDefaults.standard.set(tokenResult, forKey:"token")
-                               let storyboard = UIStoryboard(name: "Main", bundle: nil)
-                            
-                           let viewController = storyboard.instantiateViewController(withIdentifier: "MainViewController")
-                        //     self.present(viewController, animated: true, completion: nil)
-                            
-                            viewController.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem
-                            viewController.navigationItem.leftItemsSupplementBackButton = true
+                if self.loginSuccess == false {
+                    
+                    if emailTxt.text != "" && passwordTxt.text != ""  {
+                        let params: Parameters = [
+                            "grant_type": "password",
+                            "client_id": "1",
+                            "client_secret": "ZXdMNAizgOVs27EOqTil3lJGG9z4TIwDy2GdfGxR",
+                            "username": emailTxt.text!,
+                            "password": passwordTxt.text!            ]
+                        
+                        let headers: HTTPHeaders = ["Accept": "application/json"]
+                        
+                        
+                        Alamofire.request(urlString + "oauth/token", method: .post, parameters: params, encoding: JSONEncoding.default, headers: headers).validate().responseJSON { (response:DataResponse<Any>) in
+                            if response.result.isSuccess {
+                                if let result:[String:Any] = response.result.value as? [String:Any] {
+                                    let tokenResult = result["access_token"] as? String
+                                    print("token  : " + tokenResult!)
+                                    
+                                    self.loginSuccess = true
+                                    
+                                    UserDefaults.standard.set(tokenResult, forKey:"token")
+                                    
+                                    self.performSegue(withIdentifier: "login", sender: self)
+                                    
+                                }
+                                
+                            }else {
+                                self.errorLbl.text = "Indentifiants invalides"
+                                
+                            }
                         }
                         
-                    }else {
-                        self.errorLbl.text = "Indentifiants invalides"
-
+                    }else{
+                        self.errorLbl.text = "Veuillez saisir vos identifiants"
                     }
+                    
                 }
                 
-            }else{
-                errorLbl.text = "Veuillez saisir vos identifiants"
             }
-
-            
+            if ident == "subscribe" {
+                print("subscribe...shouldPerformSegue...")
+                self.loginSuccess = true
+            }
         }
+        
+        return self.loginSuccess
+    }
+    
+    
+    @IBAction func logoutTouched(_ sender: UIButton) {
+        UserDefaults.standard.setValue(nil, forKey: "token")
+        self.showLogin()
+        self.loginSuccess = false
     }
 
     
---
+++ betSwift2/SubscibeController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  betSwift2
+//
+//  Created by stagiaire on 21/03/2017.
+//  Copyright © 2017 stagiaire. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class SubscribeController: UIViewController {
+    
+    @IBOutlet weak var inputName: UITextField!
+    @IBOutlet weak var inputEmail: UITextField!
+    @IBOutlet weak var inputPassword: UITextField!
+    @IBOutlet weak var btnSubmit: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
+    @IBAction func submitTouched(_ sender: UIButton) {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
